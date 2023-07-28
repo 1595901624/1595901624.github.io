6f8bda616f169f8c81f697088949b68b1d0be867
haoyu <haoyu3@163.com>
2023-07-28 18:30:26 +0800

    init project

--- docs/.vitepress/theme/index.ts
+++
@@ -1,5 +0,0 @@
-import BlogTheme from '@sugarat/theme'
-// 自定义样式重载
-import './style.scss'
-
-export default BlogTheme
---
+++ index.js
@@ -0,0 +1,45 @@
+const axios = require('axios')
+const fs = require('fs')
+
+/**
+ * 搜索md文档中的图片地址
+ */
+function search_image() {
+    // 读取md文档
+    fs.readFile('README.md', 'utf-8', function (err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            // 正则表达式
+            const reg = /!\[.*?\]\((.*?)\)/g
+            // 匹配结果
+            const result = data.match(reg)
+            // 遍历匹配结果
+            for (let i = 0; i < result.length; i++) {
+                // 获取图片地址
+                const url = result[i].replace(reg, '$1')
+                // 下载图片
+                download_image(url)
+            }
+        }
+    })
+}
+
+/**
+ * 下载图片
+ * @param url 图片地址
+ */
+function download_image(url) {
+    // 使用axios下载图片
+    // https://gitee.com/haoyu3/photo_gallery/raw/master/rust/001/rustc%E5%91%BD%E4%BB%A4.png
+    axios.get(url, {
+        responseType: 'arraybuffer'
+    }).then(function (response) {
+        // 将图片存入到本地
+        fs.writeFile('rustc命令.png', response.data, function (err) {
+            if (err) {
+                console.log(err)
+            }
+        })
+    })
+}
